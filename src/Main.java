5c11a3b802e882cbcf404492685c2ae97d36138c
Victor Reis David <victorreis5919@gmail.com>
2026-06-15 11:54:54 -0300

    main teste

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
-import View.HospitalView;
+import View.users.RegistryView;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        HospitalView hospital = new HospitalView();
-        hospital.iniciar();
+        RegistryView view = new RegistryView();
 
+        view.Saudar();
+
+        while (true) {
+            view.MenuInicial(sc);
+        }
     }
 
 }
